6656f9cda914d8552fa4c3fdf1ac6d266f1a6ef9
illia-bi <34r4fr44f43@gmail.com>
2026-05-17 10:10:38 +0300

    add input handling

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-print("Project initialized")
-
 def process_event(event_name):
     return f"Processing event: {event_name}"
 
 
-print(process_event("login"))
+event = input("Enter event name: ")
+
+print(process_event(event))

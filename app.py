e2014d41df82e84dc257e1e9bb7be5451fceb578
illia-bi <34r4fr44f43@gmail.com>
2026-05-17 10:45:35 +0300

    main: change error message

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def main():
     if validate_event(event):
         print(process_event(event))
     else:
-        print("ERROR! Invalid event")
+        print("MAIN BRANCH ERROR")
 
 
 if __name__ == "__main__":
